19ad901998714320581f209b474def54c3445c83
Fasina Farook <146752536+fasinafarook@users.noreply.github.com>
2025-07-28 22:43:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/userRoutes/userRoute');
 const adminRoutes = require('./routes/adminRoutes/adminRoute');
-
 const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,21 +19,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: 'https://matbakh-bayti.vercel.app', 
-    credentials: true, 
+    credentials: true,
   })
 );
 
+// Routes
 app.use('/api/user', userRoutes);
 app.use('/api/admin', adminRoutes);
 
+// Error handler middleware
+app.use(errorHandler);
 
-app.use(errorHandler)
-
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
